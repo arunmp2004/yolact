0597f95860c21cfd5108494ddeab32843c19721a
Arun M P <bat@MacBook-Pro-4.local>
2020-03-07 01:04:07 -0600

    First Commit1

--- eval.py
+++ eval.py
@@ -1,4 +1,4 @@
-# Testing my changes are here.
+# Testing my changes are here. 
 from data import COCODetection, get_label_map, MEANS, COLORS
 from yolact import Yolact
 from utils.augmentations import BaseTransform, FastBaseTransform, Resize
